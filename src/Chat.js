e0750a82bd506db2048cc8ff94be6d7a68c51d7c
Adhitia Cahya Nugraha <adhit.cnugraha@gmail.com>
2022-12-04 05:56:00 +0700

    timezone change and ui updated

--- src/Chat.js
+++ src/Chat.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./Chat.css";
 import { AttachFile, MoreVert, SearchOutlined } from "@mui/icons-material";
 import { Avatar, IconButton } from "@mui/material";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
+import MicIcon from "@mui/icons-material/Mic";
+import moment from "moment";
 
 function Chat() {
   return (
@@ -31,20 +34,34 @@ function Chat() {
         <p className="chat__message">
           <span className="chat__name">Nizar</span>
           Yo, dude
-          <span className="chat__timestamp">{new Date().toGMTString()}</span>
+          <span className="chat__timestamp">{moment().format("hh:mm")}</span>
         </p>
 
         <p className="chat__message">
           <span className="chat__name">Resti</span>
           is this WacApp? it's seems like Whats*** i guess.
-          <span className="chat__timestamp">{new Date().toGMTString()}</span>
+          <span className="chat__timestamp">{moment().format("hh:mm")}</span>
         </p>
 
         <p className="chat__message">
           <span className="chat__name">Cindy</span>I think its quite better than
           the original one, haha~
-          <span className="chat__timestamp">{new Date().toGMTString()}</span>
+          <span className="chat__timestamp">{moment().format("hh:mm")}</span>
         </p>
+
+        <p className="chat__message chat__receiver">
+          <span className="chat__name">Adhit</span>maybe, yes HAHAHA
+          <span className="chat__timestamp">{moment().format("hh:mm")}</span>
+        </p>
+      </div>
+
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input placeholder="Type message" type="text" />
+          <button type="submit">Send a message</button>
+        </form>
+        <MicIcon />
       </div>
     </div>
   );
