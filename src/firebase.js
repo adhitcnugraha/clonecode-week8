ba062dfa1680103edf3c4280b1dc79d50172469d
Adhitia Cahya Nugraha <adhit.cnugraha@gmail.com>
2022-12-04 00:03:49 +0700

    First update and update dependencies

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAa1-nuaK1zwk6ArZUibgPKcPShqjQB4qU",
+  authDomain: "clonecode-week8.firebaseapp.com",
+  projectId: "clonecode-week8",
+  storageBucket: "clonecode-week8.appspot.com",
+  messagingSenderId: "224311382266",
+  appId: "1:224311382266:web:566ffe325d26474a903943",
+};
