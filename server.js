58f441618ab697c7db68e42fb19d73153a091f44
Adhitia Cahya Nugraha <adhit.cnugraha@gmail.com>
2022-12-05 01:24:09 +0700

    backend update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import Messages from "./dbMessages.js";
 import Pusher from "pusher";
+import cors from "cors";
 
 // App Config
 const app = express();
@@ -9,12 +10,7 @@ const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  next();
-});
+app.use(cors());
 
 // DB Config
 const connection_url =
