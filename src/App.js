1f9cdbc0ae120fd5a26af3df6107d3c16993edef
Adhitia Cahya Nugraha <adhit.cnugraha@gmail.com>
2022-12-05 04:37:30 +0700

    frontend updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
+import Pusher from "pusher-js";
+import axios from "./axios";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    axios.get("/messages/sync").then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+
+  console.log(messages);
+
+  useEffect(() => {
+    const pusher = new Pusher("c0bc7defd41eb8bdcfed", {
+      cluster: "ap1",
+    });
+
+    const channel = pusher.subscribe("messages");
+    channel.bind("got inserted", (data) => {
+      alert(JSON.stringify(data));
+    });
+  }, []);
+
   return (
     <div className="app">
       <div className="app__body">
